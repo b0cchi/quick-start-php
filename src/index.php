0adfc9e47beac7ad4229b528fc189ef06b862dbd
b0cchi <b0cchi@outlook.jp>
2022-07-05 13:27:24 +0900

    first commit

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo '<h1>Quick start PHP</h1>'; ?> 
+        <div>
+            <?php phpinfo(); ?>
+        </div>
+    </body>
+</html>
